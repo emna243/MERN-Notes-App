22208ed0d94b16b905867a4b9b646e206b5993a3
emna243 <190292570+emna243@users.noreply.github.com>
2026-07-29 10:41:49 -0700

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoute from "./routes/notesRoute.js";
 import {connectDB} from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
+
 dotenv.config();
 //const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5001;
+const ___dirname = path.resolve();
 
-
-app.use(cors({
-    origin : "http://localhost:5173",
-})); // this middleware will allow cross origin requests
-
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin : "http://localhost:5173",
+    })); // this middleware will allow cross origin requests
+}
 // middleware
 app.use(express.json());// this middleware will parse json bodies
 app.use(rateLimiter);
@@ -30,6 +33,14 @@ app.use(rateLimiter);
 app.use("/api/notes",notesRoute);
 //Endpoint is a combnation between a URL and a HTTP method (GET, POST, PUT, DELETE)
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(___dirname,"../frontend/dist")));
+
+    app.use("*", (req,res) => {
+        res.sendFile(path.join(___dirname,"../frontend/dist/index.html"));
+    })
+
+}
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("Server started on PORT : ", PORT);
